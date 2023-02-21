3dc86b59a5c115f172c13c08a93243a0ae6e2271
kurumi021222 <125748627+kurumi021222@users.noreply.github.com>
2023-02-21 18:16:54 +0800

    Add files via upload

---
+++ week1.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+
+<meta charset="UTF-8">
+<title>高大資管讚喇</title>
+
+</head>    
+
+<body>
+    <center>
+    <?php
+        echo"您是第\n";
+    ?>
+    <table border="0" cellspacing="0" cellpadding="0"><tr><td align="center"><a href="http://www.rays-counter.com/"><img src="http://www.rays-counter.com/d1262_f3_022/63f4986ab4c7e/" alt="アクセスカウンター" border="0"></a></td></tr><tr><td align="center"><img src="http://www.rays-counter.com/images/counter_01.gif" border="0"><img src="http://www.rays-counter.com/images/counter_02.gif" border="0"><img src="http://www.rays-counter.com/images/counter_03.gif" border="0"><img src="http://www.rays-counter.com/images/counter_04.gif" border="0" ><img src="http://www.rays-counter.com/images/counter_05.gif" border="0"></td></tr></table>
+    <?php
+        echo"位拜訪者\n";
+    ?>
+    </center>
+</body>
+</html>
